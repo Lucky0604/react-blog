976ea66b089b4ff09cb0635741fa26c99af9e656
Lucky <175917645@qq.com>
2016-04-29 00:25:36 +0800

    back end basic setup

---
+++ back-end/api.js
@@ -0,0 +1,28 @@
+var express = require('express'),
+    path = require('path'),
+    logger = require('morgan'),
+    bodyParser = require('body-parser'),
+    cookieSession = require('cookies-session'),
+    app = express();
+
+/**
+ * 全局变量：
+ * C 配置
+ * M 数据Model
+ * F 方法
+ */
+
+global.C = require('./config');
+global.M = {};
+global.F = require(path.join(C.dir.controller, C.exceptFolder, 'funcs'));
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieSession(C.cookieSession));
+
+require(path.join(C.dir.model, C.exceptFolder));        // model 初始化入口
+require(path.join(C.dir.controller, C.exceptFolder)) (app);          // router 初始化入口
+
+// 监听端口
+app.listen(C.port);
---
+++ back-end/config/dev.js
@@ -0,0 +1,27 @@
+var path = require('path'),
+    beRoot = path.resolve(__dirname, '..'),
+    envConfig = require('../../env.config').dev.apiServer,
+    config = {
+        cookieSession: {
+            name: 'blog',
+            keys: ["Lucky's blog"]
+        },
+        db: {       // 数据库配置
+            uri: 'mongodb: //localhost:27017/blog',
+            opts: {
+                user: '',
+                pass: ''
+            }
+        },
+        port: envConfig.port,   // 程序端口
+        dir: {
+            root: beRoot,
+            model: path.resolve(beRoot, 'models'),
+            controller: path.resolve(beRoot, 'controllers'),
+            resource: path.resolve(beRoot, '../resource')
+        },
+        resourceFixUrl: '',         // 静态资源web访问修正路径
+        exceptFolder: 'except'      // model 和 controller中read dir排除的目录名称
+    };
+
+module.exports = config;
