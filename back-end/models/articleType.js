f270748c3fd130999474f98596f85689deb2f1c0
Lucky <175917645@qq.com>
2016-04-29 23:20:52 +0800

    add models

---
+++ back-end/models/articleType.js
@@ -0,0 +1,9 @@
+module.exports = function() {
+    return {
+        level: Number,          // 层级
+        parent: String,
+        name: String,
+        path: String,
+        enabled: Boolean
+    }
+}
