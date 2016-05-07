8383cca7456396067d9614a4a7542564125166b5
Lucky <175917645@qq.com>
2016-05-07 10:35:25 +0800

    add admin redux config

---
+++ front-end/src/redux/middleware/admin/article.js
@@ -0,0 +1,9 @@
+import createCRUD from '../../../helpers/createCRUD';
+
+const {methods: {create, update, load, del}, createReducer} = createCRUD('admin/article', 'CRUD');
+
+export default function reducer(state = {}, action = {}) {
+    return createReducer(state, action) || state;
+}
+
+export {create, update, load, del};
