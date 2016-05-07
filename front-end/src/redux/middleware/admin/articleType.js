5b6db012314c31db9be8907f2a95ae9b76007e78
Lucky <175917645@qq.com>
2016-05-07 11:05:22 +0800

    add article redux config and auth state config

---
+++ front-end/src/redux/middleware/admin/articleType.js
@@ -0,0 +1,9 @@
+import createCRUD from '../../../helpers/createCRUD';
+
+const {methods: {create, update, load, del}, createReducer} = createCRUD('admin/detail', 'CRUD', 'articleType');
+
+export default function reducer(state = {}, action = {}) {
+    return createReducer(state, action) || state;
+}
+
+export {create, update, load, del};
---
+++ front-end/src/redux/middleware/admin/articleTypeList.js
@@ -0,0 +1,9 @@
+import createCRUD from '../../../helpers/createCRUD';
+
+const {methods: {load}, createReducer} = createCRUD('admin/list', 'R', 'articleType');
+
+export default function reducer(state = {}, action = {}) {
+    return createReducer(state, action) || state;
+}
+
+export {load};
